cccb2851115cd8e66238f9efaab365bad17fef41
ghot <gregoire.hot@gmail.com>
2016-12-12 17:09:02 +0100

    exemple of form with validators

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,7 +1,11 @@
-import { Component, ViewChild  } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+//Validators
+import { AgeValidator } from  '../../validators/age';
+import { UsernameValidator } from  '../../validators/username';
 
 /*
   Generated class for the Form page.
@@ -15,13 +19,60 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormPage {
 
-@ViewChild('signupSlider') signupSlider: any;
+  @ViewChild('signupSlider') signupSlider: any;
 
-  constructor(public navCtrl: NavController) { }
+  slideOneForm: FormGroup;
+  slideTwoForm: FormGroup;
+
+  submitAttempt: boolean = false;
+
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+
+    this.slideOneForm = formBuilder.group({
+        firstName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        age: ['', AgeValidator.isValid]
+    });
+ 
+    this.slideTwoForm = formBuilder.group({
+        username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')]), UsernameValidator.checkUsername],
+        privacy: ['', Validators.required],
+        bio: ['']
+    });
+
+  }
+
+
+  next() {
+    this.signupSlider.slideNext();
+  }
+
+  prev() {
+    this.signupSlider.slidePrev();
+  }
+
+  save(){
+ 
+    this.submitAttempt = true;
+ 
+    if(!this.slideOneForm.valid){
+        this.signupSlider.slideTo(0);
+    } 
+    else if(!this.slideTwoForm.valid){
+        this.signupSlider.slideTo(1);
+    }
+    else {
+        console.log("success!")
+        console.log(this.slideOneForm.value);
+        console.log(this.slideTwoForm.value);
+    }
+ 
+}
+
 
   ionViewDidLoad() {
     console.log('Hello FormPage Page');
-    
+
   }
 
 }
